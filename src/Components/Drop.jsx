9191f4ad5e12331e4a7fccb06c17184e6b7fe418
Sumit Singh <124269664+Rusty-98@users.noreply.github.com>
2024-02-13 23:34:24 +0530

    hidden added

--- src/Components/Drop.jsx
+++ src/Components/Drop.jsx
@@ -2,7 +2,7 @@
 const Drop = ({region}) => {
   return (
     <select className="border-4 border-black rounded-lg p-2 bg-transparent" onChange={(e) => region(e.target.value)}>
-        <option className="bg-[#313131] rounded-xl text-[#f0fff8]" value="">Filter by Area</option>
+        <option className="bg-[#313131] rounded-xl text-[#f0fff8]" value="" hidden>Filter by Area</option>
         <option className="bg-[#313131] rounded-xl text-[#f0fff8]" value="Africa">Africa</option>
         <option className="bg-[#313131] rounded-xl text-[#f0fff8]" value="America">America</option>
         <option className="bg-[#313131] rounded-xl text-[#f0fff8]" value="Asia">Asia</option>
